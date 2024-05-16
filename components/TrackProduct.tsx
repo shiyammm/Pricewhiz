7e9ab8a8dca3621dcaba07209ed98f47ed215c2e
shiyammm <122890678+shiyammm@users.noreply.github.com>
2024-05-16 23:15:47 +0530

    Add Track Product component for price tracking with email notifications

---
+++ components/TrackProduct.tsx
@@ -0,0 +1,73 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { FormEvent, useState } from 'react';
+import { addUserEmail } from '@/lib/action';
+
+export function TrackProduct({ productId: string }) {
+  const [email, setEmail] = useState('');
+  const [isTracking, setIsTracking] = useState(false);
+
+  const handleEmailSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setIsTracking(false);
+    await addUserEmail(productId, email);
+    setEmail('');
+  };
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="default" className="text-md font-semibold">
+          Track Product
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-md">
+        <DialogHeader>
+          <DialogTitle>Enter your Email</DialogTitle>
+          <DialogDescription>
+            Stay updated with product pricing alerts right in your inbox!{' '}
+          </DialogDescription>
+        </DialogHeader>
+        <form
+          className="flex items-center space-x-2"
+          onSubmit={handleEmailSubmit}
+        >
+          <div className="grid flex-1 gap-2">
+            <Label htmlFor="link" className="sr-only">
+              Email
+            </Label>
+            <Input
+              id="link"
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <Button type="submit" size="sm" className="px-3">
+            Track
+          </Button>
+        </form>
+        <DialogFooter className="sm:justify-start">
+          <DialogClose asChild>
+            <Button type="button" variant="secondary">
+              {isTracking ? 'Tracking' : 'Track'}
+            </Button>
+          </DialogClose>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
