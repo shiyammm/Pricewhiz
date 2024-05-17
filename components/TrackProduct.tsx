3a611043d26543bf00b81cf953dd2972c65654d3
shiyammm <122890678+shiyammm@users.noreply.github.com>
2024-05-17 23:17:33 +0530

    Add open and close functionality to trackProducts component

--- components/TrackProduct.tsx
+++ components/TrackProduct.tsx
@@ -16,58 +16,78 @@ import { Label } from '@/components/ui/label';
 import { FormEvent, useState } from 'react';
 import { addUserEmail } from '@/lib/action';
 
-export function TrackProduct({ productId: string }) {
+type Props = {
+  productId: string;
+};
+
+export function TrackProduct({ productId }: Props) {
   const [email, setEmail] = useState('');
   const [isTracking, setIsTracking] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const open = () => {
+    setIsOpen(true);
+  };
+
+  const close = () => {
+    setIsOpen(false);
+  };
 
   const handleEmailSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     setIsTracking(false);
     await addUserEmail(productId, email);
     setEmail('');
+    close();
   };
 
   return (
     <Dialog>
       <DialogTrigger asChild>
-        <Button variant="default" className="text-md font-semibold">
+        <Button
+          variant="default"
+          className="text-md font-semibold"
+          onClick={open}
+        >
           Track Product
         </Button>
       </DialogTrigger>
-      <DialogContent className="sm:max-w-md">
-        <DialogHeader>
-          <DialogTitle>Enter your Email</DialogTitle>
-          <DialogDescription>
-            Stay updated with product pricing alerts right in your inbox!{' '}
-          </DialogDescription>
-        </DialogHeader>
-        <form
-          className="flex items-center space-x-2"
-          onSubmit={handleEmailSubmit}
-        >
-          <div className="grid flex-1 gap-2">
-            <Label htmlFor="link" className="sr-only">
-              Email
-            </Label>
-            <Input
-              id="link"
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </div>
-          <Button type="submit" size="sm" className="px-3">
-            Track
-          </Button>
-        </form>
-        <DialogFooter className="sm:justify-start">
-          <DialogClose asChild>
-            <Button type="button" variant="secondary">
+      {isOpen && (
+        <DialogContent className="sm:max-w-md">
+          <DialogHeader>
+            <DialogTitle>Enter your Email</DialogTitle>
+            <DialogDescription>
+              Stay updated with product pricing alerts right in your inbox!{' '}
+            </DialogDescription>
+          </DialogHeader>
+          <form
+            className="flex items-center space-x-2"
+            onSubmit={handleEmailSubmit}
+          >
+            <div className="grid flex-1 gap-2">
+              <Label htmlFor="link" className="sr-only">
+                Email
+              </Label>
+              <Input
+                id="link"
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <Button type="submit" size="sm" className="px-3">
               {isTracking ? 'Tracking' : 'Track'}
             </Button>
-          </DialogClose>
-        </DialogFooter>
-      </DialogContent>
+          </form>
+          <DialogFooter className="sm:justify-start">
+            <DialogClose asChild>
+              <Button type="button" variant="secondary">
+                Close
+              </Button>
+            </DialogClose>
+          </DialogFooter>
+        </DialogContent>
+      )}
     </Dialog>
   );
 }
