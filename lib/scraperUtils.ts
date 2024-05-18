81e8d3e615da93c30d2d5b4c9dd87fe793ea4f66
shiyammm <122890678+shiyammm@users.noreply.github.com>
2024-05-18 15:01:16 +0530

    feat : Implement mailing functionality using nodemailer

--- lib/scraperUtils.ts
+++ lib/scraperUtils.ts
@@ -1,6 +1,17 @@
+import { Product } from './types';
+
 type PriceList = {
   price: number;
 };
+
+const Notification = {
+  WELCOME: 'WELCOME',
+  CHANGE_OF_STOCK: 'CHANGE_OF_STOCK',
+  LOWEST_PRICE: 'LOWEST_PRICE',
+  THRESHOLD_MET: 'THRESHOLD_MET',
+};
+
+const THRESHOLD_PERCENTAGE = 40;
 
 export function extractDescription($: any) {
   const elements = $('#feature-bullets .a-unordered-list .a-spacing-mini span');
@@ -63,3 +74,22 @@ export function getAveragePrice(priceList: PriceList[]) {
 
   return averagePrice;
 }
+
+export const getEmailNotifType = (
+  scrapedProduct: Product,
+  currentProduct: Product,
+) => {
+  const lowestPrice = getLowestPrice(currentProduct.priceHistory);
+
+  if (scrapedProduct.currentPrice < lowestPrice) {
+    return Notification.LOWEST_PRICE as keyof typeof Notification;
+  }
+  if (!scrapedProduct.isOutOfStock && currentProduct.isOutOfStock) {
+    return Notification.CHANGE_OF_STOCK as keyof typeof Notification;
+  }
+  if (scrapedProduct.discountRate >= THRESHOLD_PERCENTAGE) {
+    return Notification.THRESHOLD_MET as keyof typeof Notification;
+  }
+
+  return null;
+};
